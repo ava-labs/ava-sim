7e41c67f802e5afa34deea8ea69f0c117bd2ede6
Ceyhun Onur <ceyhun.onur@avalabs.org>
2021-10-19 23:55:09 +0300

    move genesis path to timestampvm script

--- scripts/timestampvm.sh
+++ scripts/timestampvm.sh
@@ -5,6 +5,7 @@ MAIN_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )"; cd .. && pwd )
 source "$MAIN_PATH"/scripts/constants.sh
 
 # Create genesis
+timestamp_genesis_path="${build_dir}/timestampvm/genesis.txt"
 touch $timestamp_genesis_path
 echo "fP1vxkpyLWnH9dD6BQA" > $timestamp_genesis_path
 

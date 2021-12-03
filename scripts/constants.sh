fbf529d4dc78fad95b721ff044f5a6bc184dd1d8
Ceyhun Onur <ceyhun.onur@avalabs.org>
2021-12-04 01:00:35 +0300

    bump coreth

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -4,5 +4,5 @@ build_dir="${MAIN_PATH}/build"
 timestampvm_version='v1.2.0'
 timestampvm_path="${build_dir}/timestampvm/timestampvm"
 
-coreth_version='v0.8.0-rc.3'
+coreth_version='v0.8.1-rc.0'
 evm_path="${build_dir}/system-plugins/evm"
